a7bd38ab2e938fc137fb2950485af07bb4d97437
JL Granda <jorge_granda82112@elpoli.edu.co>
2018-09-10 20:17:42 -0500

    La interfaz ya intercambia las letras, falta implementar el modulo lab

--- canvas.py
+++ canvas.py
@@ -1,9 +1,31 @@
-from tkinter import * 
+from tkinter import Button, Canvas, Tk, PhotoImage
+
+def show_j():
+    print("Debe mmotrar la J")
+    img = PhotoImage(file="letter_J.png")
+    c.create_image(100,120, anchor="center", image=img)
+    c.img = img
+
+def show_a():
+    print("Debe mmotrar la A")
+    img = PhotoImage(file="letter_A.png")
+    c.create_image(110,120, anchor="center", image=img)
+    c.img = img
+
+
+def show_d():
+    print("Debe mmotrar la D")
+    img = PhotoImage(file="letter_D.png")
+    c.create_image(100,120, anchor="center", image=img)
+    c.img = img
 
 ventana = Tk()
-c = Canvas(ventana, width=500,height=500)
-ventana.geometry("600x600")
-c.pack()
-img = PhotoImage(file="letter_A.png")
-c.create_image(100,100, anchor=NW, image=img)
+ventana.geometry("400x300")
+ventana.title("Laboratorio 5!")
+c = Canvas(ventana, width=600,height=600)
+c.place(x=150,y=20)
+btnJ = Button(ventana, text="Mostrar la J", command=show_j).place(x=20,y=20)
+btnA = Button(ventana, text="Mostrar la A", command=show_a).place(x=20,y = 60)
+btnD = Button(ventana, text="Mostrar la D", command=show_d).place(x= 20, y=100)
+btnExit = Button(ventana, text="Salir", command=ventana.quit).place(x= 20, y=200)
 ventana.mainloop()
