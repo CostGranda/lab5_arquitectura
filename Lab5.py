a81176b644eb7118d00dcdbabeff5dda38248938
unknown <CST G>
2018-09-13 11:18:32 -0500

    It works!!

--- Lab5.py
+++ Lab5.py
@@ -1,109 +1,82 @@
-import RPi.GPIO as GPIO
+from hvanegas import show_g, show_a, show_n, show_d, show_r, clean, leds_off, show_l, show_z, show_c, show_o, show_j, show_s, setup_pin
 from time import sleep
 
-# Columnas
-COL1 = 26
-COL2 = 19
-COL3 = 13
-COL4 = 6
-COL5 = 5
-
-# Filas
-ROW1 = 21
-ROW2 = 20
-ROW3 = 16
-ROW4 = 12
-ROW5 = 1
-ROW6 = 7
-ROW7 = 8
-columns = [COL1, COL2, COL3, COL4, COL5]
-rows = [ROW1, ROW2, ROW3, ROW4, ROW5, ROW6, ROW7]
+setup_pin()
 
 
-def setup_leds():
-    """Configura los leds en modo salida"""
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(columns, GPIO.OUT)
-    GPIO.setup(rows, GPIO.OUT)
+def show_granda():
+    try:
+        for i in range(2):
+            leds_off()
+            show_g()
+            sleep(0.1)
+            leds_off()
+            show_r()
+            sleep(0.1)
+            leds_off()
+            show_a()
+            sleep(0.1)
+            leds_off()
+            show_n()
+            sleep(0.1)
+            leds_off()
+            show_d()
+            sleep(0.1)
+            leds_off()
+            show_a()
+            sleep(0.1)
+            leds_off()
+    except KeyboardInterrupt:
+        clean()
 
 
-def leds_off():
-    """Apaga todos los leds"""
-    GPIO.output(columns, GPIO.HIGH)
-    GPIO.output(rows, GPIO.HIGH)
+def show_alcaraz():
+    try:
+        for i in range(2):
+            leds_off()
+            show_a()
+            sleep(0.1)
+            leds_off()
+            show_l()
+            sleep(0.1)
+            leds_off()
+            show_c()
+            sleep(0.1)
+            leds_off()
+            show_a()
+            sleep(0.1)
+            leds_off()
+            show_r()
+            sleep(0.1)
+            leds_off()
+            show_a()
+            sleep(0.1)
+            leds_off()
+            show_z()
+            sleep(0.1)
+            leds_off()
+    except KeyboardInterrupt:
+        clean()
 
 
-setup_leds()
-
-
-def show_j():
-    # Para la J
-    leds_off()
-    GPIO.output(COL1, GPIO.HIGH)
-    GPIO.output([ROW2, ROW3, ROW7], GPIO.LOW)
-    sleep(0.1)
-    leds_off()
-    GPIO.output(COL2, GPIO.HIGH)
-    GPIO.output([ROW1, ROW7], GPIO.LOW)
-    sleep(0.1)
-    leds_off()
-
-    GPIO.output(COL3, GPIO.HIGH)
-    GPIO.output([ROW1, ROW7], GPIO.LOW)
-    sleep(0.1)
-    leds_off()
-
-    GPIO.output(COL4, GPIO.HIGH)
-    GPIO.output([ROW1, ROW7], GPIO.LOW)
-    sleep(0.1)
-    leds_off()
-
-    GPIO.output(COL5, GPIO.HIGH)
-    GPIO.output([ROW2, ROW3, ROW4, ROW5, ROW6, ROW7], GPIO.LOW)
-    sleep(0.1)
-    leds_off()
-
-
-def show_a():
-    leds_off()
-    GPIO.output(COL1, GPIO.HIGH)
-    GPIO.output([ROW1, ROW2, ROW3, ROW4, ROW5, ROW6], GPIO.LOW)
-    sleep(0.1)
-    leds_off()
-    GPIO.output(COL2, GPIO.HIGH)
-    GPIO.output([ROW3, ROW7], GPIO.LOW)
-    sleep(0.1)
-    leds_off()
-    GPIO.output(COL3, GPIO.HIGH)
-    GPIO.output([ROW3, ROW7], GPIO.LOW)
-    sleep(0.1)
-    leds_off()
-    GPIO.output(COL4, GPIO.HIGH)
-    GPIO.output([ROW3, ROW7], GPIO.LOW)
-    sleep(0.1)
-    leds_off()
-    GPIO.output(COL5, GPIO.HIGH)
-    GPIO.output([ROW1, ROW2, ROW3, ROW4, ROW5, ROW6], GPIO.LOW)
-
-
-
-def show_d():
-    leds_off()
-    GPIO.output(COL1, GPIO.HIGH)
-    GPIO.output([ROW1, ROW2, ROW3, ROW4, ROW5, ROW6,ROW7], GPIO.LOW)
-    sleep(0.1)
-    leds_off()
-    GPIO.output(COL2, GPIO.HIGH)
-    GPIO.output([ROW1, ROW7], GPIO.LOW)
-    sleep(0.1)
-    leds_off()
-    GPIO.output(COL3, GPIO.HIGH)
-    GPIO.output([ROW1, ROW7], GPIO.LOW)
-    sleep(0.1)
-    leds_off()
-    GPIO.output(COL4, GPIO.HIGH)
-    GPIO.output([ROW1, ROW7], GPIO.LOW)
-    sleep(0.1)
-    leds_off()
-    GPIO.output(COL5, GPIO.HIGH)
-    GPIO.output([ROW1, ROW2, ROW3, ROW4, ROW5, ROW6], GPIO.LOW)
+def show_rojas():
+    try:
+        for i in range(2):
+            leds_off()
+            show_r()
+            sleep(0.1)
+            leds_off()
+            show_o()
+            sleep(0.1)
+            leds_off()
+            show_j()
+            sleep(0.1)
+            leds_off()
+            show_a()
+            sleep(0.1)
+            leds_off()
+            show_s()
+            sleep(0.1)
+            leds_off()
+    except KeyboardInterrupt:
+        clean()
