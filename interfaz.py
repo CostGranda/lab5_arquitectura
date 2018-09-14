675af18a576ff7dbd425710920eab234ece3ccbd
JL Granda <jorge_granda82112@elpoli.edu.co>
2018-09-13 19:46:55 -0500

    works works

--- interfaz.py
+++ interfaz.py
@@ -5,7 +5,7 @@ from abcdario import show_g, show_a, show_n, show_d, show_r, clean, leds_off, sh
 
 
 root = tk.Tk()
-root.geometry("1200x900")
+root.geometry("1200x800")
 root.title("apellidos MATRIZ DE LEDS")
 fuente = tkinter.font.Font(family='helvetica', size=20, weight="bold")
 fuente2 = tkinter.font.Font(family='helvetica', size=12, weight="bold")
@@ -19,7 +19,6 @@ Jorge Luis Granda 1042770436
 Ingeniero Hernando Vanegas López
 """
 label = tk.Label(text=integrantes, font=fuente, bg="#000000", fg="#F4845F")
-
 label.pack(fill=tk.X)
 
 granda = [show_g, show_r, show_a, show_n, show_d, show_a]
@@ -70,7 +69,8 @@ def generar():
     apellidos = 'GRANDALCRZOJS'
     a = 0
     for letra in apellidos:
-        label[a] = tk.Button(text=letra, font=fuente2, command=letras_ind[a], bd=5)
+        label[a] = tk.Button(text=letra, font=fuente2,
+                             command=letras_ind[a], bd=5)
         label[a].place(x=px, y=py, height=70, width=70)
         px = px+100
         if(a == 5):
@@ -87,11 +87,11 @@ generar()
 lbl_resultado = tk.Label(root, text="", font=fuente3,
                          bg="#000000", fg="#F27059")
 lbl_resultado.place(x=850, y=200, height=400, width=400)
-boton_nombre = tk.Button(text="GRANDA", command=btn_granda, bd=4)
-boton_nombre.place(x=850, y=600, height=60, width=70)
-boton_apellido1 = tk.Button(text="ALCARAZ", command=btn_alcaraz, bd=4)
-boton_apellido1.place(x=950, y=600, height=60, width=90)
-boton_apellido2 = tk.Button(text="ROJAS", command=btn_rojas, bd=4)
-boton_apellido2.place(x=1050, y=600, height=60, width=100)
+boton_granda = tk.Button(text="GRANDA", command=btn_granda, bd=4)
+boton_granda.place(x=850, y=610, height=60, width=70)
+boton_alcaraz = tk.Button(text="ALCARAZ", command=btn_alcaraz, bd=4)
+boton_alcaraz.place(x=950, y=610, height=60, width=90)
+boton_rojas = tk.Button(text="ROJAS", command=btn_rojas, bd=4)
+boton_rojas.place(x=1050, y=610, height=60, width=100)
 
 root.mainloop()
