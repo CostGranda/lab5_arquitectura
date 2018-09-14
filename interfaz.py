511d2731cecd3b722b6b8fdb64c1dcde163cddd6
JL Granda <jorge_granda82112@elpoli.edu.co>
2018-09-13 19:26:03 -0500

    botones apellidos

--- interfaz.py
+++ interfaz.py
@@ -1,13 +1,13 @@
 import tkinter as tk
-#from abcdario import show_g, show_a, show_n, show_d, show_r, clean, leds_off, show_l, show_z, show_c, show_o, show_j, show_s, setup_pin
+from abcdario import show_g, show_a, show_n, show_d, show_r, clean, leds_off, show_l, show_z, show_c, show_o, show_j, show_s, setup_pin
 import tkinter.font
 from time import sleep
 #import Lab5 
 
 
 root = tk.Tk()
-root.geometry("2000x900")
-root.title("APELLIDOS MATRIZ DE LEDS")
+root.geometry("1200x900")
+root.title("apellidos MATRIZ DE LEDS")
 fuente=tkinter.font.Font(family='helvetica',size=20,weight="bold")
 fuente2=tkinter.font.Font(family='helvetica',size=12,weight ="bold")
 fuente3=tkinter.font.Font(family='helvetica',size=120,weight ="bold")
@@ -19,51 +19,54 @@ Jorge Luis Granda 1042770436
 
 Ingeniero Hernando Vanegas López
 """
-label = tk.Label(text="Nombre: Alexander Quintero Rosso 1037645440 \n" +
-"       Sebastian Rivera Giraldo\n"+
-"      Ramiro Andres Bedoya Gallon\n"+
-"Ingeniero Hernando Vanegas López\n "+
-"Matriz de leds",font=fuente,bg="#000000",fg="#007ACC")
+label = tk.Label(text=integrantes,font=fuente,bg="#000000",fg="#F4845F")
 
 label.pack(fill=tk.X)
 
+granda = {"G":show_g, "R": show_r,"A": show_a, "N":show_n, "D":show_d, "A":show_a}
 
 def btn_granda():
-  apellido_quintero= ['Q','U','I','N','T','E','R','O','']
-  for i in range(0,9,1):
-    lbl_resultado['text']=apellido_quintero[i]
-    sleep(1)
+  apellido_granda= ['G','R','A','N','D','A','']
+  for letra in apellido_granda:
+    lbl_resultado['text']= letra
+    sleep(0.1)
     root.update_idletasks()
-  
+    granda[letra]()
+    leds_off()
 
+alcaraz = {"A":show_a, "L": show_l,"C": show_c, "A":show_a, "R":show_r, "A":show_a, "Z": show_z}
 
 def btn_alcaraz():
-  apellido_rivera = ['R','I','V','E','R','A','']
-  for i in range(0,7,1):
-    lbl_resultado['text']=apellido_rivera[i]
-    sleep(1)
+  apellido_alcaraz = ['A','L','C','A','R','A','Z', '']
+  for letra in apellido_alcaraz:
+    lbl_resultado['text']=letra
+    sleep(0.1)
     root.update_idletasks()
+    alcaraz[letra]()
+    leds_off()
 
-
+rojas = {"R": show_r, "O": show_o, "J": show_j, "A": show_a, "S": show_s}
 def btn_rojas():
-  apellido_bedoya = ['B','E','D','O','Y','A','']
-  for i in range(0,7,1):
-    lbl_resultado['text']=apellido_bedoya[i]
+  apellido_rojas = ['R','O','J','A','S','']
+  for letra in apellido_rojas:
+    lbl_resultado['text']=letra
     sleep(1)
     root.update_idletasks()
+    rojas[letra]()
+    leds_off()
 
 
 def generar():
   px=50
-  py=200
-  label = ['Q','U','I','N','T','E','R','O','V','A','B','D','Y']
-  Apellidos='QUINTEROVABDY'
+  py=210
+  label = ['G','R','A','N','D','A','L','C','R','Z','O','J','S']
+  apellidos='GRANDALCRZOJS'
   a=0
-  for letra in Apellidos:
-    label[a]= tk.Button(text = letra,font=fuente2)
+  for letra in apellidos:
+    label[a]= tk.Button(text = letra,font=fuente2, bd=5)
     label[a].place(x=px,y=py,height=70, width=70)
     px=px+100
-    if(a==7):
+    if(a==5):
       px=50
       py=py+100
     elif(a==9):
@@ -74,13 +77,13 @@ def generar():
 
 generar()
 
-lbl_resultado=tk.Label(root,text="",font=fuente3,bg="#000000",fg="#007ACC")
+lbl_resultado=tk.Label(root,text="",font=fuente3,bg="#000000",fg="#F27059")
 lbl_resultado.place(x=850,y=200,height=400,width=400)
-boton_nombre=tk.Button(text="QUINTERO",command=btn_granda)
+boton_nombre=tk.Button(text="GRANDA",command=btn_granda, bd=4)
 boton_nombre.place(x=850,y=600,height=60,width=70)
-boton_apellido1=tk.Button(text="RIVERA",command=btn_alcaraz)
+boton_apellido1=tk.Button(text="ALCARAZ",command=btn_alcaraz, bd=4)
 boton_apellido1.place(x=950,y=600,height=60,width=90)
-boton_apellido2=tk.Button(text="BEDOYA",command=btn_rojas)
+boton_apellido2=tk.Button(text="ROJAS",command=btn_rojas,bd=4)
 boton_apellido2.place(x=1050,y=600,height=60,width=100)
 
 root.mainloop()
